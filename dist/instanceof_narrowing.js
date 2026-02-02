da32eef6e8c565f9dc5f1734c37a1dfa3bb0c08a
PAZZO123 <smbabazipatrick@gmail.com>
2026-02-02 18:44:10 +0200

    Adding quiz interface

---
+++ dist/instanceof_narrowing.js
@@ -0,0 +1,8 @@
+"use strict";
+function logValue(x) {
+    if (x instanceof Date) {
+        console.log(x.toUTCString());
+    }
+    console.log(x);
+}
+//# sourceMappingURL=instanceof_narrowing.js.map

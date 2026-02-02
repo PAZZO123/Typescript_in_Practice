2d044f53150286b9c019cc5a78bda20de103457e
PAZZO123 <smbabazipatrick@gmail.com>
2026-02-02 18:57:26 +0200

    Adding quiz interface

--- dist/as_narrowing.js
+++ dist/as_narrowing.js
@@ -2,4 +2,12 @@
 function isFish(pet) {
     return pet.swim !== undefined;
 }
+function GetFood(pet) {
+    if (isFish(pet)) {
+        pet;
+        return 'fish food';
+    }
+    pet;
+    return "bird food";
+}
 //# sourceMappingURL=as_narrowing.js.map
--- src/as_narrowing.ts
+++ src/as_narrowing.ts
@@ -1,6 +1,15 @@
 type Fish={swim:()=>void}
 type Bird={fly:()=>void}
 
-function isFish(pet: Fish|Bird){
+function isFish(pet: Fish|Bird) : pet is Fish{
     return (pet as Fish).swim !== undefined
+}
+
+function GetFood(pet: Fish| Bird){
+    if(isFish(pet)) {
+        pet
+        return 'fish food'
+    }
+    pet 
+    return "bird food"
 }

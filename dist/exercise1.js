3b644bbcf6141137864c6d6d960d5d18c7a3cee4
PAZZO123 <smbabazipatrick@gmail.com>
2026-02-05 19:15:17 +0200

    Adding this to Github

---
+++ dist/exercise1.js
@@ -0,0 +1,27 @@
+"use strict";
+// Write a TypeScript function processValue that takes string | number | boolean | null | undefined, uses type guards to log uppercase strings
+// , numbers fixed to two decimals, and boolean as "True"/"False,” and throws an error 
+// for null or undefined, including a type assertion to narrow the type.
+function processValue(value) {
+    if (value === null) {
+        throw new Error('Invaid Type');
+    }
+    else if (typeof value === 'string') {
+        let str = value;
+        console.log(str.toUpperCase());
+        return;
+    }
+    else if (typeof value === 'number') {
+        let num = value;
+        console.log(num);
+    }
+    else if (typeof value === 'boolean') {
+        let bool = value;
+        console.log(bool ? "True" : "False");
+    }
+}
+processValue(true);
+processValue(12);
+processValue("name");
+//processValue(null)
+//# sourceMappingURL=exercise1.js.map
